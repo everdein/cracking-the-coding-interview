2a4c7744e9ab7bfac0091936cfe78fd1572d0c23
Matthew <everdein@gmail.com>
2025-01-11 15:51:18 -0500

    add StringCompression algorithm to perform basic string compression using character counts

---
+++ src/chapter_1/section_1/Question_6_StringCompression.java
@@ -0,0 +1,14 @@
+package chapter_1.section_1;
+
+// Problem 1.6: String Compression
+// Prompt:
+// Implement a method to perform basic string compression using the counts of repeated characters. 
+// For example, the string "aabcccccaaa" would become "a2b1c5a3". If the compressed string would not 
+// become smaller than the original string, your method should return the original string. You can 
+// assume the string has only uppercase and lowercase letters (a-z).
+
+public class Question_6_StringCompression {
+    public static void main(String[] args) {
+
+    }
+}
