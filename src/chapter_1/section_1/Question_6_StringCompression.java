50ae872a206c566189a5b1bb265ef8939f7a7f8f
Matthew <everdein@gmail.com>
2025-01-15 22:40:01 -0500

    add RotateMatrix class to implement 90-degree rotation of an n x n matrix

--- src/chapter_1/section_1/Question_6_StringCompression.java
+++ src/chapter_1/section_1/Question_6_StringCompression.java
@@ -19,32 +19,43 @@ public class Question_6_StringCompression {
         System.out.println(compressString("")); // ""
     }
 
+    // TODO: Implement a method utilizing StringBuilder to compress the string
     public static String compressString(String str) {
         int firstPointer = 0;
         int secondPointer = 0;
         String compressedString = "";
 
+        // If the string is empty or has only one character, return the string
         if (str.length() == 0) {
             return str;
         }
 
+        // If the string has only one character, return the string
         if (str.length() == 1) {
             return str;
         }
 
         while (secondPointer < str.length()) {
+            // If the characters at the first and second pointers are the same, increment
+            // the second pointer
             if (str.charAt(firstPointer) == str.charAt(secondPointer)) {
                 secondPointer++;
-            } else if (str.charAt(firstPointer) != str.charAt(secondPointer)) {
+            }
+            // If the characters at the first and second pointers are different, add the
+            // character at the first pointer
+            else if (str.charAt(firstPointer) != str.charAt(secondPointer)) {
                 compressedString = compressedString + str.charAt(firstPointer) + (secondPointer - firstPointer);
                 firstPointer = secondPointer;
             }
 
+            // If the second pointer reaches the end of the string, add the character at the
+            // first pointer and the count of the character
             if (secondPointer == str.length()) {
                 compressedString = compressedString + str.charAt(firstPointer) + (secondPointer - firstPointer);
             }
         }
 
+        // Return the compressed string if it is shorter than the original string
         return compressedString.length() < str.length() ? compressedString : str;
     }
 }
---
+++ src/chapter_1/section_1/Question_7_RotateMatrix.java
@@ -0,0 +1,12 @@
+package chapter_1.section_1;
+
+// Problem 1.7: Rotate Matrix
+// Prompt:
+// Given an image represented by an 𝑛 × 𝑛
+// n×n matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+public class Question_7_RotateMatrix {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
